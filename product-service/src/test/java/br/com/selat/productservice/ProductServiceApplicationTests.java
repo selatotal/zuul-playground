665401c00cd58b1ab6464cfa9d4325b5590ccf13
Tales Viegas <tales.viegas@ilegra.com>
2019-12-18 16:29:47 -0300

    Initial commit

---
+++ product-service/src/test/java/br/com/selat/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.selat.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
